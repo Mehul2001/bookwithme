cfc0cc290b6819c8effe5fd2f56e41ead75bc26c
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2019-03-29 16:47:26 -0700

    Adds manage rental and booking endpoint and add  delete rental endpoint

--- server/controllers/booking.js
+++ server/controllers/booking.js
@@ -42,7 +42,20 @@ exports.createBooking = function (req, res) {
 
                 return res.status(422).send({ errors: [{ title: 'Invalid Booking!', detail: 'Choosen dates are already taken!' }] });
             }
-        })
+        });
+}
+
+exports.getUserBookings = function (req, res) {
+    const user = res.locals.user;
+    Booking
+        .where({ user })
+        .populate('rental')
+        .exec(function (err, foundBookings) {
+            if (err) {
+                return res.status(422).send({ errors: normalizeErrors(err.errors) });
+            }
+            return res.json(foundBookings);
+        });
 }
 
 function isValidBooking(proposedBooking, rental) {
